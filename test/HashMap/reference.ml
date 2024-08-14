1bf690d7363e7efb0416711fd424287ae28b4257
François Pottier <francois.pottier@inria.fr>
2024-08-14 15:54:29 +0200

    Add test/HashMap.

---
+++ test/HashMap/reference.ml
@@ -0,0 +1,94 @@
+(******************************************************************************)
+(*                                                                            *)
+(*                                   Hachis                                   *)
+(*                                                                            *)
+(*                       François Pottier, Inria Paris                        *)
+(*                                                                            *)
+(*       Copyright 2024--2024 Inria. All rights reserved. This file is        *)
+(*       distributed under the terms of the GNU Library General Public        *)
+(*       License, with an exception, as described in the file LICENSE.        *)
+(******************************************************************************)
+
+(* A reference implementation of hash maps. *)
+
+module Make (K : sig
+  type t
+  val hash  : t -> int
+  val equal : t -> t -> bool
+end)
+(V : sig type t end)
+= struct
+
+  module H = Hashtbl.Make(K)
+
+  type map =
+    V.t H.t
+
+  let create () : map =
+    H.create 32
+
+  let mem m x =
+    H.mem m x
+
+  let find_key m x =
+    (* [find_key] is not directly supported by OCaml's hash tables.
+       We use an inefficient method based on iteration. *)
+    let exception Found of K.t in
+    try
+      H.iter (fun y _v ->
+        if K.equal x y then raise (Found y)
+      ) m;
+      raise Not_found
+    with Found y ->
+      y
+
+  let find_value m x =
+    H.find m x
+
+  (* OCaml's hash tables implement multi-maps: an element can be
+     present several times. We must compensate for this, and not
+     add an element if it is already in the map. *)
+
+  let add m x v =
+    let was_present = H.mem m x in
+    let was_absent = not was_present in
+    if was_absent then
+      H.add m x v;
+    was_absent
+
+  let add_absent m x v =
+    let was_present = H.mem m x in
+    assert (not was_present);
+    H.add m x v
+
+  let find_key_else_add m x v =
+    try
+      find_key m x
+    with Not_found ->
+      add_absent m x v;
+      raise Not_found
+
+  let remove m x =
+    let y = find_key m x in
+    H.remove m x;
+    y
+
+  let population m =
+    H.length m
+
+  let cleanup _m =
+    ()
+
+  let clear =
+    H.clear
+
+  let reset =
+    H.reset
+
+  let copy =
+    H.copy
+
+  let iter f m =
+    H.iter (fun x _ -> f x) m
+
+end
---
+++ test/HashMap/test.ml
@@ -0,0 +1,152 @@
+(******************************************************************************)
+(*                                                                            *)
+(*                                   Hachis                                   *)
+(*                                                                            *)
+(*                       François Pottier, Inria Paris                        *)
+(*                                                                            *)
+(*       Copyright 2024--2024 Inria. All rights reserved. This file is        *)
+(*       distributed under the terms of the GNU Library General Public        *)
+(*       License, with an exception, as described in the file LICENSE.        *)
+(******************************************************************************)
+
+open Monolith
+
+module A = struct
+  include Array
+  type element = int
+  type t = element array
+  let empty = [||]
+end
+
+(* We use nonnegative integer elements, and equip them with an equivalence
+   relation that is not equality. We view two integer elements as equivalent
+   if they are equal except possibly in their least significant bit. *)
+module V = struct
+  type t = int
+  let normalize x = x lor 1
+  let equal x y = Int.equal (normalize x) (normalize y)
+  let hash x = Hashtbl.hash (normalize x)
+end
+
+(* We use -1 and -2 as sentinels. Our generators must be careful not
+   to produce these elements. *)
+module S = struct type t = int let void = (-1) let tomb = (-2) end
+
+module R = Reference.Make(V)(V)
+module C = Hachis.HashMap.Make(A)(S)(V)(A)
+
+(* -------------------------------------------------------------------------- *)
+
+(* Print the above prologue when showing a failing test scenario. *)
+
+let () = dprintf "          \
+          #require \"hector\"
+          module A = struct
+            include Array
+            type element = int
+            type t = element array
+            let empty = [||]
+          end
+          module S = struct type t = int let void = (-1) let tomb = (-2) end
+          module V = struct
+            type t = int
+            let normalize x = x lor 1
+            let equal x y = Int.equal (normalize x) (normalize y)
+            let hash x = Hashtbl.hash (normalize x)
+          end
+          open Hachis.HashMap.Make(A)(S)(V)(A)
+"
+
+(* -------------------------------------------------------------------------- *)
+
+(* We have one abstract type, namely [map]. *)
+
+let check _model =
+  C.check, constant "check"
+
+let map =
+  declare_abstract_type ~check ()
+
+(* We draw random non-sentinel (nonnegative) integer keys. *)
+
+let key =
+  semi_open_interval 0 32
+
+(* We draw random integer values. *)
+
+let value =
+  semi_open_interval 0 32
+
+(* The absence of a key in a set
+   is a precondition of [add_absent]. *)
+
+let absent (s : R.map) (x : int) : bool =
+  not (R.mem s x)
+
+(* We test [iter] by converting it to an [elements] function.
+   This function must produce a sorted list, because the order
+   in which elements are produced is unspecified. *)
+
+let elements_of_iter iter v =
+  let xs = ref [] in
+  iter (fun x -> xs := x :: !xs) v;
+  List.sort Int.compare !xs
+
+(* -------------------------------------------------------------------------- *)
+
+(* Declare the operations. *)
+
+let () =
+
+  let spec = unit ^> map in
+  declare "create" spec R.create C.create;
+
+  let spec = map ^> key ^> bool in
+  declare "mem" spec R.mem C.mem;
+
+  let spec = map ^> key ^!> key in
+  declare "find_key" spec R.find_key C.find_key;
+
+  let spec = map ^> key ^!> value in
+  declare "find_value" spec R.find_value C.find_value;
+
+  let spec = map ^> key ^> value ^> bool in
+  declare "add" spec R.add C.add;
+
+  let spec = map ^>> fun s -> (absent s) % key ^> value ^> unit in
+  declare "add_absent" spec R.add_absent C.add_absent;
+
+  let spec = map ^> key ^> value ^!> key in
+  declare "find_key_else_add" spec R.find_key_else_add C.find_key_else_add;
+
+  let spec = map ^> key ^!> key in
+  declare "remove" spec R.remove C.remove;
+
+  let spec = map ^> int in
+  declare "population" spec R.population C.population;
+
+  let spec = map ^> unit in
+  declare "cleanup" spec R.cleanup C.cleanup;
+
+  let spec = map ^> unit in
+  declare "clear" spec R.clear C.clear;
+
+  let spec = map ^> unit in
+  declare "reset" spec R.reset C.reset;
+
+  let spec = map ^> map in
+  declare "copy" spec R.copy C.copy;
+
+  let spec = map ^> list key in
+  declare "elements_of_iter iter" spec
+    (elements_of_iter R.iter) (elements_of_iter C.iter);
+
+  ()
+
+(* -------------------------------------------------------------------------- *)
+
+(* Start the engine! *)
+
+let () =
+  let fuel = 128 in
+  main fuel
