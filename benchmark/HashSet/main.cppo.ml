5508a8db71af9aebbe52f3015280f374367ccfe9
François Pottier <francois.pottier@inria.fr>
2024-08-20 19:35:39 +0200

    More architecture.

--- benchmark/HashSet/main.cppo.ml
+++ benchmark/HashSet/main.cppo.ml
@@ -91,13 +91,6 @@ module BabyWSet = struct
   let[@inline] create () = ref empty
   let[@inline] add s x = (s := add x !s)
   let[@inline] remove s x = (s := remove x !s)
-  let[@inline] mem s x = mem x !s
-  let is_empty s = is_empty !s
-  let choose s =
-    let n = cardinal !s in
-    assert (n > 0);
-    let i = Random.int n in
-    get !s i
 end
 
 (* We use this Set module in the generation of benchmark scenarios. *)
@@ -135,22 +128,22 @@ end
 type key =
   V.t
 
-(* An instruction type is one of the following: [add absent], [add present],
-   [remove absent], [remove present]. *)
+(* An instruction type is one of the following. *)
 
-type presence =
-  | Absent
-  | Present
+type argument =
+  | Key of key (* a specific key *)
+  | Absent     (* any currently absent key *)
+  | Present    (* any currently present key *)
 
 type itype =
-  | Add of presence
-  | Remove of presence
+  | ITAdd of argument
+  | ITRemove of argument
 
 (* A concrete instruction is [add x] or [remove x]. *)
 
 type instruction =
-  | Add of key
-  | Remove of key
+  | IAdd of key
+  | IRemove of key
 
 (* A scenario consists of two sequences of instructions.
    The first sequence is not timed, and can be used to
@@ -168,16 +161,18 @@ type scenario =
 type recipe =
   int * (int -> itype)
 
-(* [choose_key p s u] randomly chooses an integer value inside or
-   outside of the set [s], depending on [p]. If it must be chosen
+(* [choose_key a s u] randomly chooses an integer value inside or
+   outside of the set [s], depending on [a]. If it must be chosen
    outside of [s], then it is randomly drawn below [u]. *)
 
-let rec choose_key (p : presence) s u : key =
-  match p with
+let rec choose_key (a : argument) s u : key =
+  match a with
+  | Key x ->
+      x
   | Absent ->
       let x = Random.int u in
       if R.mem s x then
-        choose_key p s u (* retry *)
+        choose_key a s u (* retry *)
       else
         x
   | Present ->
@@ -194,14 +189,14 @@ let rec choose_key (p : presence) s u : key =
 
 let choose_instruction s u (it : itype) : instruction =
   match it with
-  | Add p ->
-      let x = choose_key p s u in
+  | ITAdd a ->
+      let x = choose_key a s u in
       R.add s x;
-      Add x
-  | Remove p ->
-      let x = choose_key p s u in
+      IAdd x
+  | ITRemove a ->
+      let x = choose_key a s u in
       R.remove s x;
-      Remove x
+      IRemove x
 
 (* [choose_sequence s u r] randomly chooses an instruction sequence
    that obeys the recipe [r]. *)
@@ -217,8 +212,8 @@ let print_statistics (seq : sequence) =
   let add, remove = ref 0, ref 0 in
   Array.iter (fun instruction ->
     match instruction with
-    | Add    _ -> incr add
-    | Remove _ -> incr remove
+    | IAdd    _ -> incr add
+    | IRemove _ -> incr remove
   ) seq;
   printf "This scenario involves %d insertions and %d deletions.\n%!"
     !add !remove
@@ -229,7 +224,7 @@ let print_statistics (seq : sequence) =
    (initialization). The recipe [r2] is used to generate a second sequence of
    instructions (use). *)
 
-let choose_scenario s u (r1, r2 : recipe * recipe) : scenario =
+let choose_scenario u (r1, r2 : recipe * recipe) : scenario =
   let s = R.create() in
   let seq1 = choose_sequence s u r1 in
   R.reset_max_pop s;
@@ -251,27 +246,27 @@ let choose_scenario s u (r1, r2 : recipe * recipe) : scenario =
 #define EXECUTE(seq, add, remove) \
   for i = 0 to Array.length seq - 1 do \
     match Array.unsafe_get seq i with \
-    | Add x -> \
+    | IAdd x -> \
         ignore (add s x) \
-    | Remove x -> \
+    | IRemove x -> \
         ignore (remove s x) \
   done
 
-(* [BENCHMARK(name, scenario, create, add, remove)] expands to
+(* [BENCHMARK(name, scenario, M)] expands to
    a benchmark whose name is [name], obeying [scenario], using
-   the operations [create], [add], [remove]. *)
+   the operations provided by the module [M]. *)
 
-#define BENCHMARK(_name, _scenario, create, add, remove) \
+#define BENCHMARK(_name, _scenario, M) \
 ( \
   let scenario = _scenario in \
   let seq1, seq2 = scenario in \
   let basis = Array.length seq2 \
   and name = _name \
   and run () = \
-    let s = create () in \
-    EXECUTE(seq1, add, remove); \
+    let s = M.create () in \
+    EXECUTE(seq1, M.add, M.remove); \
     fun () -> \
-      EXECUTE(seq2, add, remove) \
+      EXECUTE(seq2, M.add, M.remove) \
   in \
   B.benchmark ~name ~quota ~basis ~run \
 )
@@ -280,23 +275,18 @@ let choose_scenario s u (r1, r2 : recipe * recipe) : scenario =
 
 (* Specific (pairs of) recipes. *)
 
-(* -------------------------------------------------------------------------- *)
+let empty : recipe =
+  0, (fun _ -> assert false)
 
-(* Sequential insertions. *)
+(* Consecutive insertions: starting with an empty set, successively insert
+   all integers from [0] to [n-1]. *)
 
-#define SEQADD(n, candidate, create, add) \
-( \
-  let basis = n \
-  and name = sprintf "add (consecutive data, n = %d) (%s)" n candidate \
-  and run () () = \
-    let s = create () in \
-    for i = 0 to n-1 do \
-      let x = i in \
-      ignore (add s x) \
-    done \
-  in \
-  B.benchmark ~name ~quota ~basis ~run \
-)
+(* In this benchmark, [u] is irrelevant. *)
+
+let consecutive_insertions n u : scenario =
+  let seq1 = empty
+  and seq2 = n, (fun i -> ITAdd (Key i)) in
+  choose_scenario u (seq1, seq2)
 
 (* -------------------------------------------------------------------------- *)
 
@@ -329,12 +319,16 @@ let randadd_data =
 
 let adds n =
   let u = n in
+  let name candidate = sprintf "add (consecutive data, n = %d) (%s)" n candidate in
+  let scenario = consecutive_insertions n u in
   [
-    SEQADD(n, "Set", Set.create, Set.add);
-    SEQADD(n, "Baby.W.Set", BabyWSet.create, BabyWSet.add);
-    SEQADD(n, "Hashtbl", Hashtbl.create, Hashtbl.add);
-    SEQADD(n, "HashSet", HashSet.create, HashSet.add);
-    SEQADD(n, "HashMap", HashMap.create, HashMap.add);
+    BENCHMARK(name "Set", scenario, Set);
+    BENCHMARK(name "Baby.W.Set", scenario, BabyWSet);
+    BENCHMARK(name "Hashtbl", scenario, Hashtbl);
+    BENCHMARK(name "HashSet", scenario, HashSet);
+    BENCHMARK(name "HashMap", scenario, HashMap);
+  ] @
+  [
     RANDADD(n, u, "Set", Set.create, Set.add);
     RANDADD(n, u, "Baby.W.Set", BabyWSet.create, BabyWSet.add);
     RANDADD(n, u, "Hashtbl", Hashtbl.create, Hashtbl.add);
