1e5bf5b2df0577cb4d93b2ffc4f605452f2d49de
François Pottier <francois.pottier@inria.fr>
2024-08-26 18:52:17 +0200

    Documentation: comments and organization into sections.

--- src/Signatures.cppo.ml
+++ src/Signatures.cppo.ml
@@ -81,16 +81,40 @@ module type SET = sig
   (**The type of the elements of a set. *)
   type element
 
-  (**The type of sets. A set [s] always contains at most one element
-     in each equivalence class: that is, [mem s x] and [mem s y] and
-     [equiv x y] imply [x = y]. *)
+  (**The type of sets. At all times, a set [s] contains at most one element of
+     each equivalence class: that is, [mem s x] and [mem s y] and [equiv x y]
+     imply [x = y]. *)
   type set
+
+  (** {2 Creation} *)
 
   (**[create()] creates a fresh empty set. *)
   val create : unit -> set
 
-  (**[mem s x] determines whether [x], or some equivalent element, is a
-     member of the set [s]. *)
+  (**[copy s] returns a new set whose elements are the elements of [s].
+     Its time complexity is linear in the size of the internal data array. *)
+  val copy : set -> set
+
+  (** {2 Insertion} *)
+
+  (**If [x] or some equivalent element is a member of the set [s], then
+     [add s x] has no effect and returns [false]. Otherwise, [add s x]
+     inserts the element [x] into the set [s] and returns [true]. *)
+  val add : set -> element -> bool
+
+  (**[add_absent s x] inserts the element [x] into the set [s]. No
+     result is returned. {b The element [x], or an element [y] that is
+     equivalent to [x], must not already be a member of [s]}: otherwise,
+     the data structure would become inconsistent. It is recommended to
+     guard this operation with [assert (not (mem s x))]. This allows the
+     code to be both safe (when runtime assertions are enabled) and
+     efficient (when runtime assertions are disabled). *)
+  val add_absent : set -> element -> unit
+
+  (** {2 Lookup} *)
+
+  (**[mem s x] determines whether the element [x], or some element [y] that
+     is equivalent to [x], is a member of the set [s]. *)
   val mem : set -> element -> bool
 
   (**[find s x] determines whether some element [y] that is equivalent to
@@ -98,15 +122,7 @@ module type SET = sig
      [Not_found] is raised. *)
   val find : set -> element -> element
 
-  (**If [x] or some equivalent element is a member of the set [s], then
-     [add s x] has no effect and returns [false]. Otherwise, [add s x]
-     inserts the element [x] into the set [s] and returns [true]. *)
-  val add : set -> element -> bool
-
-  (**[add_absent s x] inserts the element [x] into the set [s]. [x], or
-     some equivalent element, must not already be a member of [s]. No
-     result is returned. *)
-  val add_absent : set -> element -> unit
+  (** {2 Insertion and lookup} *)
 
   (**[find_else_add s x] determines whether some element [y] that is equivalent
      to [x] is a member of the set [s]. If so, [y] is returned. Otherwise, the
@@ -116,22 +132,34 @@ module type SET = sig
      [try find s x with Not_found -> add_absent s x; raise Not_found]. *)
   val find_else_add : set -> element -> element
 
-  (**If some element [y] that is equivalent to [x] is a member of the set
-     [s], then [remove s x] removes [y] from the set [s] and returns
-     [y]. Otherwise, this call has no effect and raises [Not_found]. *)
+  (** {2 Deletion} *)
+
+  (**If some element [y] that is equivalent to [x] is a member of the
+     set [s], then [remove s x] removes [y] from the set [s] and returns
+     [y]. Otherwise, the set [s] is unaffected, and [Not_found] is
+     raised. *)
   val remove : set -> element -> element
+
+  (** {2 Iteration} *)
+
+  (**[foreach_key f s] applies the user-supplied function [f] in turn to
+     each element [x] of the set [s]. {b The function [f] must not
+     modify the set [s]}: that is, no elements can be inserted or
+     deleted while iteration is ongoing. *)
+  val foreach_key : (element -> unit) -> set -> unit
+
+  (**[iter] is a synonym for [foreach_key]. *)
+  val iter : (element -> unit) -> set -> unit
+
+  (** {2 Cardinality} *)
 
   type population = int
 
-  (**[population s] returns the number of inhabitants of the set [s]. *)
+  (**[population s] returns the cardinality of the set [s],
+     that is, the number of inhabitants of this set. *)
   val population : set -> population
 
-  (**[cleanup s] cleans up the internal representation of the set [s] by
-     freeing the space occupied by any previously removed elements. If
-     many elements have been recently removed from the set, this can free
-     up some space and delay the need to grow the set's internal data
-     array. *)
-  val cleanup : set -> unit
+  (** {2 Management} *)
 
   (**[clear s] empties the set [s]. The internal data array is retained,
      and is erased. The time complexity of this operation is linear in
@@ -142,25 +170,26 @@ module type SET = sig
      The time complexity of this operation is constant. *)
   val reset : set -> unit
 
-  (**[copy s] returns a new set whose elements are the elements of [s].
-     Its time complexity is linear in the size of the internal data array. *)
-  val copy : set -> set
-
-  (**[foreach_key f s] applies the user-supplied function [f] in turn to
-     each element [x] of the set [s]. *)
-  val foreach_key : (element -> unit) -> set -> unit
-
-  (**[iter] is a synonym for [foreach_key]. *)
-  val iter : (element -> unit) -> set -> unit
-
-  (**[show show_key s] returns a textual representation of the set
-     [s]. The user-supplied function [show_key] is used to obtain a
-     textual representation of each element. *)
-  val show : (element -> string) -> set -> string
+  (**[cleanup s] cleans up the internal representation of the set [s] by
+     freeing the space occupied by any previously removed elements. If
+     many elements have been recently removed from the set, this can
+     free up some space and delay the need to grow the set's internal
+     data array. The time complexity of this operation is linear in the
+     size of the internal data array. *)
+  val cleanup : set -> unit
 
   (**[statistics s] returns a string of information about the population,
      capacity and occupancy of the set [s]. *)
   val statistics : set -> string
+
+  (** {2 Display} *)
+
+  (**[show show_key s] returns a textual representation of the set [s].
+     This representation is delimited with curly braces. Two consecutive
+     elements are separated with a comma and a space. The user-supplied
+     function [show_key] is used to obtain a textual representation of
+     each element. *)
+  val show : (element -> string) -> set -> string
 
   (**/**)
   (* In debug builds, [check s] checks that the set's internal invariant
@@ -177,16 +206,43 @@ module type MAP = sig
   (**The type of values. *)
   type value
 
-  (**The type of maps. A map [m] always contains at most one key in
-     each equivalence class: that is, [mem m x] and [mem m y] and
-     [equiv x y] imply [x = y]. *)
+  (**The type of maps. A map can be viewed as a set of pairs [(x, v)] of a
+     key [x] and a value [v]. When a pair [(x, v)] exists in the map [m],
+     we say that {i the key [x] is present with value [v]} in the map [m].
+     At all times, a map [m] contains at most one key of each equivalence
+     class: that is, [mem m x] and [mem m y] and [equiv x y] imply [x = y]. *)
   type map
+
+  (** {2 Creation} *)
 
   (**[create()] creates a fresh empty map. *)
   val create : unit -> map
 
-  (**[mem m x] determines whether the key [x], or some equivalent key,
-     is present in the map [m]. *)
+  (**[copy m] returns a new map whose key-value bindings are those of [m].
+     Its time complexity is linear in the size of the internal data arrays. *)
+  val copy : map -> map
+
+  (** {2 Insertion} *)
+
+  (**If [x] or some equivalent element is present in the map [m], then
+     [add m x v] has no effect and returns [false]. Otherwise, [add m x v]
+     inserts the key [x] with value [v] into the map [m] and returns
+     [true]. *)
+  val add : map -> key -> value -> bool
+
+  (**[add_absent m x v] inserts the key [x] with value [v] into the map
+     [m]. No result is returned. {b The key [x], or a key [y] that is
+     equivalent to [x], must not already be present in [m]}: otherwise,
+     the data structure would become inconsistent. It is recommended to
+     guard this operation with [assert (not (mem m x))]. This allows the
+     code to be both safe (when runtime assertions are enabled) and
+     efficient (when runtime assertions are disabled). *)
+  val add_absent : map -> key -> value -> unit
+
+  (** {2 Lookup} *)
+
+  (**[mem m x] determines whether the key [x], or some key [y] that is
+     equivalent to [x], is present in the map [m]. *)
   val mem : map -> key -> bool
 
   (**[find_key m x] determines whether some key [y] that is equivalent
@@ -199,16 +255,7 @@ module type MAP = sig
      returned. Otherwise, [Not_found] is raised. *)
   val find_value : map -> key -> value
 
-  (**If [x] or some equivalent element is present in the map [m], then
-     [add m x v] has no effect and returns [false]. Otherwise, [add m x v]
-     inserts the key [x] with value [v] into the map [m] and returns
-     [true]. *)
-  val add : map -> key -> value -> bool
-
-  (**[add_absent m x v] inserts the key [x] with value [v] into the map
-     [m]. [x], or some equivalent key, must not already be present in [m].
-     No result is returned. *)
-  val add_absent : map -> key -> value -> unit
+  (** {2 Insertion and lookup} *)
 
   (**[find_key_else_add m x] determines whether some key [y] that is
      equivalent to [x] is present in the map [m]. If so, [y] is returned.
@@ -219,21 +266,40 @@ module type MAP = sig
      [try find_key m x v with Not_found -> add_absent m x v; raise Not_found]. *)
   val find_key_else_add : map -> key -> value -> key
 
+  (** {2 Deletion} *)
+
   (**If some key [y] that is equivalent to [x] is present in the map
      [m], then [remove m x] removes [y] from the map [m] and returns
-     [y]. Otherwise, this call has no effect and raises [Not_found]. *)
+     [y]. Otherwise, the map [m] is unaffected, and [Not_found] is
+     raised. *)
   val remove : map -> key -> key
+
+  (** {2 Iteration} *)
+
+  (**[foreach_key f m] applies the user-supplied function [f] in turn to
+     each key [x] in the map [m]. {b The function [f] must not modify
+     the map [m]}: that is, no key-value pairs can be inserted or
+     deleted while iteration is ongoing. *)
+  val foreach_key : (key -> unit) -> map -> unit
+
+  (**[foreach_key_value f m] applies the user-supplied function [f] in
+     turn to each pair of a key [x] and value [v] in the map [m]. {b The
+     function [f] must not modify the map [m]}: that is, no key-value
+     pairs can be inserted or deleted while iteration is ongoing. *)
+  val foreach_key_value : (key -> value -> unit) -> map -> unit
+
+  (**[iter] is a synonym for [foreach_key_value]. *)
+  val iter : (key -> value -> unit) -> map -> unit
+
+  (** {2 Cardinality} *)
 
   type population = int
 
-  (**[population m] returns the number of inhabitants of the map [m]. *)
+  (**[population m] returns the cardinality of the map [m],
+     that is, the number of inhabitants of this map. *)
   val population : map -> population
 
-  (**[cleanup m] cleans up the internal representation of the map [m] by
-     freeing the space occupied by any previously removed keys. If many
-     keys have been recently removed, this can free up some space and
-     delay the need to grow the map's internal data arrays. *)
-  val cleanup : map -> unit
+  (** {2 Management} *)
 
   (**[clear m] empties the map [m]. The internal data arrays are retained,
      and are erased. The time complexity of this operation is linear in
@@ -244,30 +310,25 @@ module type MAP = sig
      The time complexity of this operation is constant. *)
   val reset : map -> unit
 
-  (**[copy m] returns a new map whose key-value bindings are those of [m].
-     Its time complexity is linear in the size of the internal data arrays. *)
-  val copy : map -> map
+  (**[cleanup m] cleans up the internal representation of the map [m] by
+     freeing the space occupied by any previously removed keys. If many
+     keys have been recently removed, this can free up some space and
+     delay the need to grow the map's internal data arrays. The time
+     complexity of this operation is linear in the size of the internal
+     data arrays. *)
+  val cleanup : map -> unit
 
-  (**[foreach_key f m] applies the user-supplied function [f] in turn to
-     each key [x] in the map [m]. *)
-  val foreach_key : (key -> unit) -> map -> unit
+  (**[statistics m] returns a string of information about the population,
+     capacity and occupancy of the map [m]. *)
+  val statistics : map -> string
 
-  (**[foreach_key_value f m] applies the user-supplied function [f] in
-     turn to each pair of a key [x] and value [v] in the map [m]. *)
-  val foreach_key_value : (key -> value -> unit) -> map -> unit
-
-  (**[iter] is a synonym for [foreach_key_value]. *)
-  val iter : (key -> value -> unit) -> map -> unit
+  (** {2 Display} *)
 
   (**[show show_key show_value m] returns a textual representation of
      the map [m]. The user-supplied functions [show_key] and
      [show_value] are used to obtain textual representations of keys
      and values. *)
   val show : (key -> string) -> (value -> string) -> map -> string
-
-  (**[statistics m] returns a string of information about the population,
-     capacity and occupancy of the map [m]. *)
-  val statistics : map -> string
 
   (**/**)
   (* In debug builds, [check m] checks that the map's internal invariant
